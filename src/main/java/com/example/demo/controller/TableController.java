4da7f490aaddcdb2bad94d4956646fd9527d2936
nantian <1308083921@qq.com>
2022-09-20 19:55:28 +0800

    springboot-thymeleaf

--- src/main/java/com/example/demo/controller/TableController.java
+++ src/main/java/com/example/demo/controller/TableController.java
@@ -5,27 +5,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TableController {
 
-    @GetMapping("/basic_table.html")
+    @GetMapping("/basic_table")
     public String basic_table(){
 
         return "table/basic_table";
     }
-    @GetMapping("/dynamic_table.html")
+    @GetMapping("/dynamic_table")
     public String dynamic_table(){
 
         return "table/dynamic_table";
     }
-    @GetMapping("/editable_table.html")
+    @GetMapping("/editable_table")
     public String editable_table(){
 
         return "table/editable_table";
     }
-    @GetMapping("/pricing_table.html")
+    @GetMapping("/pricing_table")
     public String pricing_table(){
 
         return "table/pricing_table";
     }
-    @GetMapping("/responsive_table.html")
+    @GetMapping("/responsive_table")
     public String responsive_table(){
 
         return "table/responsive_table";
