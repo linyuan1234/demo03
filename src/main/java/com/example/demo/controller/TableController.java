fc5e5d51e0c890836f384dad9a8f37e7b968a81f
nantian <1308083921@qq.com>
2022-10-12 18:03:07 +0800

    springboot-thymeleaf

--- src/main/java/com/example/demo/controller/TableController.java
+++ src/main/java/com/example/demo/controller/TableController.java
@@ -1,7 +1,14 @@
 package com.example.demo.controller;
 
+import com.example.demo.pojo.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class TableController {
 
@@ -11,7 +18,11 @@ public class TableController {
         return "table/basic_table";
     }
     @GetMapping("/dynamic_table")
-    public String dynamic_table(){
+    public String dynamic_table(Model model){
+        //表格内容的遍历
+        List<User> users = Arrays.asList(new User("刘韦", "aab"), new User("杨子", "qqq"),
+                new User("张三", "bbb"), new User("可可", "123"));
+        model.addAttribute("users",users);
 
         return "table/dynamic_table";
     }
